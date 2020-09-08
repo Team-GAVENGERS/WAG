7862bc0c465513380fb1a49c8dee9f942c4402f7
SeoungGun <saverrrw2864@naver.com>
2020-09-09 02:29:50 +0900

    상연이 음탕함 ㅅㄱ

--- app/src/main/java/gavengers/wag/MainActivity.java
+++ app/src/main/java/gavengers/wag/MainActivity.java
@@ -3,6 +3,7 @@ package gavengers.wag;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showToast("상연이는 음탕해~");
+    }
+    public void showToast(String str){
+        Toast.makeText(this.getApplicationContext(),str, Toast.LENGTH_SHORT).show();
     }
 }
