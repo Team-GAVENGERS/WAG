58b31e00eef32bf561457d6029e62efb414d55df
SeoungGun <saverrrw2864@naver.com>
2020-09-09 02:50:51 +0900

    이상한 메세지 바꿈

--- app/src/main/java/gavengers/wag/MainActivity.java
+++ app/src/main/java/gavengers/wag/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
         showToast("상연이는 음탕해~");
         showToast("싼쌈도 음탕해");
         showToast("팍태도 음탕해");
+        TextView text = findViewById(R.id.text1);
+        text.setText("팤태현!팤태현!팤태현!팤태현!팤태현!팤태현!팤태현!팤태현!");
     }
     public void showToast(String str){
         Toast.makeText(this.getApplicationContext(),str, Toast.LENGTH_SHORT).show();
