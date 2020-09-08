4614825d18d51c202eacb2e53a1c2c5873ff7b20
SeoungGun <saverrrw2864@naver.com>
2020-09-09 02:43:00 +0900

    이상한 메세지 바꿈

--- app/src/main/java/gavengers/wag/MainActivity.java
+++ app/src/main/java/gavengers/wag/MainActivity.java
@@ -3,6 +3,7 @@ package gavengers.wag;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         showToast("상연이는 음탕해~");
         showToast("싼쌈도 음탕해");
+        TextView text = findViewById(R.id.text1);
+        text.setText("팤태현!팤태현!팤태현!팤태현!팤태현!팤태현!팤태현!");
     }
     public void showToast(String str){
         Toast.makeText(this.getApplicationContext(),str, Toast.LENGTH_SHORT).show();
