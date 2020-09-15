6ae7722a4200fab1cb4c7a22101cf17b85168514
SeungGun <saverrw2864@naver.com>
2020-09-16 02:32:42 +0900

    회원가입 layout outline + 데이터 전송 준비

--- app/src/main/java/gavengers/wag/MainActivity.java
+++ app/src/main/java/gavengers/wag/MainActivity.java
@@ -3,19 +3,19 @@ package gavengers.wag;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    public EditText input_id;
+    public EditText getInput_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
-
 }
