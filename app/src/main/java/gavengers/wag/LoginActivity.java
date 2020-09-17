ca062152871dbe36ec772435abe13dc788e7ee3f
SeungGun <saverrw2864@naver.com>
2020-09-18 03:20:51 +0900

    Login - firebase linking

--- app/src/main/java/gavengers/wag/LoginActivity.java
+++ app/src/main/java/gavengers/wag/LoginActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,95 +27,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
+
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-
     FirebaseUser firebaseUser;
-
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.login_layout);
-        setContentView(R.layout.firebase_test);
+        setContentView(R.layout.login_layout);
 
-        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     public void onStart() {
         super.onStart();
-
 //        FirebaseUser currentUser = mAuth.getCurrentUser();  // 유저가 로그인이 이미 되어있나 확인
-
     }
 
-    public void regbtn_Click(View v)
-    {
-        EditText emailText = (EditText)findViewById(R.id.emailText);
-        EditText pwText = (EditText)findViewById(R.id.pwText);
-        EditText nickText = (EditText)findViewById(R.id.nickText);
 
-        SignUp(emailText.getText().toString(), pwText.getText().toString(), nickText.getText().toString());
-    }
-
-    private void SignUp(String email, String password, final String nickname) {
-        // [START create_user_with_email]
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // 로그인 성공
-                            firebaseUser = mAuth.getCurrentUser();
-
-
-                            String uid = firebaseUser.getUid();
-
-                            Map<String, Object> user = new HashMap<>();
-                            user.put("UID", uid);
-                            user.put("Nickname", nickname);
-
-                            db.collection("UserData").document(uid).set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void aVoid) {
-                                    // success
-                                    Log.d("UserData/UID", "UserData successfully written!");
-                                }
-                            })
-                                    .addOnFailureListener(new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            //fail
-                                            Log.w("UserData/UID", "UserData written failed", e);
-                                        }
-                                    });
-
-
-                        } else {
-                            // 로그인 실패
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-
-        // [END create_user_with_email]
-    }
-
-    private void SignIn(String email, String password) {
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // 성공
-                            FirebaseUser user = mAuth.getCurrentUser();
-                        } else {
-                            // 실패
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
 }
--- app/src/main/java/gavengers/wag/MainActivity.java
+++ app/src/main/java/gavengers/wag/MainActivity.java
@@ -1,20 +1,43 @@
 package gavengers.wag;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
     private Button register_btn;
+    private Button do_login;
+    private EditText email;
+    private EditText password;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+        mAuth = FirebaseAuth.getInstance();
+        do_login = findViewById(R.id.do_login);
+        email = findViewById(R.id.input_id);
+        password = findViewById(R.id.input_pw);
+        do_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignIn(email.getText().toString(),password.getText().toString());
+            }
+        });
         register_btn = findViewById(R.id.register_btn);
         register_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,5 +56,23 @@ public class MainActivity extends AppCompatActivity {
     public void showToast(String str){
         Toast.makeText(this.getApplicationContext(),str, Toast.LENGTH_SHORT).show();
     }
+    private void SignIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // 성공
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getApplicationContext(), "Login Success",
+                                    Toast.LENGTH_SHORT).show();
 
+                        } else {
+                            // 실패
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 }
