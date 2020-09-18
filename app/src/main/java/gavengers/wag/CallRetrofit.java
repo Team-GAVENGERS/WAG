414abf3dddd7de6f8c45bac4d65381fd3a5ada9b
Ji-A-Kang <jiakang@protonmail.com>
2020-09-19 04:25:21 +0900

    Retrofit nicknameCheck

--- app/src/main/java/gavengers/wag/CallRetrofit.java
+++ app/src/main/java/gavengers/wag/CallRetrofit.java
@@ -7,7 +7,13 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class CallRetrofit {
-    public boolean callNicknamecheck(String nickname){
+    public interface nicknameCheckCallback{
+        void onSucess(Model__nickname__CheckAlready result_model);
+        void onFailure();
+    }
+
+    public boolean callNicknamecheck(String nickname,
+                                     nicknameCheckCallback callback){
         boolean isRight = false;
 
         //Retrofit 호출
@@ -17,14 +23,15 @@ public class CallRetrofit {
             @Override
             public void onResponse(Call<Model__nickname__CheckAlready> call, Response<Model__nickname__CheckAlready> response) {
                 if(!response.isSuccessful()){
-                    Log.e("연결이 비정상적 : ", "error code : " + response.code());
+                    Log.e("연결이 비정상적 : ", "error code : " + response.message());
                     return;
                 }
                 Model__nickname__CheckAlready checkAlready = response.body();
                 Log.d("연결이 성공적 : ", response.body().toString());
-                if(modelCheckAlready.getResult() == "False"){
+                if(checkAlready.getResult().equals("False")){
                     Log.d("중복검사: ", "중복된 닉네임이 아닙니다");
                     modelCheckAlready.setRight(true);
+                    modelCheckAlready.setResult("True");
                 }
             }
             @Override
--- app/src/main/java/gavengers/wag/RegisterActivity.java
+++ app/src/main/java/gavengers/wag/RegisterActivity.java
@@ -28,6 +28,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 
 
@@ -74,9 +76,28 @@ public class RegisterActivity extends AppCompatActivity {
             return;
         }
 
-        CallRetrofit callRetrofit = new CallRetrofit();
-        boolean nicknameCheck = callRetrofit.callNicknamecheck(nickname);
-        Log.d("Retrofit2", "nickname check:" + String.valueOf(nicknameCheck));
+        //Retrofit nickname check
+        final Model__nickname__CheckAlready modelCheckAlready = new Model__nickname__CheckAlready(nickname);
+        Call<Model__nickname__CheckAlready> call = RetrofitClient.getApiService().nicknameCheck(modelCheckAlready);
+        call.enqueue(new Callback<Model__nickname__CheckAlready>() {
+            @Override
+            public void onResponse(Call<Model__nickname__CheckAlready> call, Response<Model__nickname__CheckAlready> response) {
+                if(!response.isSuccessful()){
+                    Log.e("연결이 비정상적 : ", "error code : " + response.message());
+                    return;
+                }
+                Model__nickname__CheckAlready checkAlready = response.body();
+                Log.d("연결이 성공적 : ", response.body().toString());
+                if(checkAlready.getResult().equals("False")){
+                    Log.d("중복검사: ", "중복된 닉네임이 아닙니다");
+                }
+            }
+            @Override
+            public void onFailure(Call<Model__nickname__CheckAlready> call, Throwable t) {
+                Log.e("연결실패", t.getMessage());
+            }
+        });
+
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
