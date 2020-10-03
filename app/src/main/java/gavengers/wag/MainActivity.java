79760b5f7e0a1e8f8dbbcf8cf2f43d67ad6ec3dd
SeungGun <saverrw2864@naver.com>
2020-10-03 14:45:52 +0900

    implemented auto-login using SharedPreferences

--- app/src/main/java/gavengers/wag/MainActivity.java
+++ app/src/main/java/gavengers/wag/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -30,7 +33,9 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseUser firebaseUser;
     public String token;
     public static Context context_main;
-
+    private CheckBox auto_login;
+    private SharedPreferences setting;
+    private SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,10 +45,38 @@ public class MainActivity extends AppCompatActivity {
         do_login = findViewById(R.id.do_login);
         email = findViewById(R.id.input_id);
         password = findViewById(R.id.input_pw);
+        auto_login = findViewById(R.id.auto_login_chkbox);
+        setting = getSharedPreferences("setting",0);
+        if(setting.getBoolean("Enabled",false)){
+            email.setText(setting.getString("email",""));
+            password.setText(setting.getString("pw",""));
+            auto_login.setChecked(true);
+        }
+        editor = setting.edit();
         do_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 SignIn(email.getText().toString(),password.getText().toString());
+            }
+        });
+        auto_login.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    String email_ = email.getText().toString();
+                    String password_ = password.getText().toString();
+                    editor.putString("email",email_);
+                    editor.putString("pw",password_);
+                    editor.putBoolean("Enabled",true);
+                    editor.commit();
+                }
+                else{
+                    editor.remove("email");
+                    editor.remove("pw");
+                    editor.remove("Enabled");
+                    editor.clear();
+                    editor.commit();
+                }
             }
         });
         register_btn = findViewById(R.id.register_btn);
