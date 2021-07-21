c6e4848c46586eca363752ea9c1d853a6bd6004e
Taehyuny <hyuny6012@naver.com>
2021-07-21 20:22:16 +0900

    Naver API position fix and marker

--- app/src/main/java/gavengers/wag/MapFragment.java
+++ app/src/main/java/gavengers/wag/MapFragment.java
@@ -1,6 +1,8 @@
 package gavengers.wag;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,11 +10,128 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
-public class MapFragment extends Fragment {
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.geometry.LatLngBounds;
+import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.NaverMapOptions;
+import com.naver.maps.map.NaverMapSdk;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.overlay.OverlayImage;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+    private com.naver.maps.map.MapFragment mapFragment;
+    private MapView mapView;
+    private static NaverMap naverMap;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setMap();
+        View v = inflater.inflate(R.layout.map_layout, container, false);
+        // 3.x API version
+        NaverMapSdk.getInstance(this.getContext()).setClient(
+                new NaverMapSdk.NaverCloudPlatformClient("eh5bexu1t1"));
+
         return inflater.inflate(R.layout.map_layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapView = view.findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
+    }
+
+    private void setMap(){
+        FragmentManager fragmentManager = getChildFragmentManager();
+        mapFragment = (com.naver.maps.map.MapFragment) fragmentManager.findFragmentById(R.id.map_view);
+
+        if(mapFragment == null) {
+            NaverMapOptions options = new NaverMapOptions()
+                    .camera(new CameraPosition(new LatLng(37.45199894842855, 127.13179114165393), 14)) // 기본 위치
+                    .logoGravity(Gravity.START | Gravity.TOP)
+                    .logoMargin(8, 8, 8, 8) // 로고 여백
+                    .locationButtonEnabled(false) // 현재 위치 비활성화
+                    .compassEnabled(false) // 나침반
+                    .zoomControlEnabled(true); // 줌
+
+            mapFragment = com.naver.maps.map.MapFragment.newInstance(options);
+            fragmentManager.beginTransaction().add(R.id.map_view, mapFragment).commit();
+
+            // 비동기 코드이기 때문에 코드 실행에 유의
+            mapFragment.getMapAsync(naverMap -> {
+                naverMap.setExtent(new LatLngBounds(new LatLng(37.44792028734633, 127.12628356183701), new LatLng(37.4570968690434, 127.13723061921826)));
+                naverMap.setMinZoom(14.0);
+                naverMap.setMaxZoom(0.0);
+                drawMarker(naverMap);
+                onMapReady(naverMap);
+            });
+
+
+        }
+    }
+
+    public void drawMarker(NaverMap naverMap){
+        Marker marker = new Marker();
+        marker.setPosition(new LatLng(37.45044136062332, 127.12986847331536));
+
+        marker.setWidth(40);
+        marker.setHeight(60);
+        Log.d("marker",marker.getPosition().toString());
+        marker.setIcon(OverlayImage.fromResource(R.drawable.marker));
+        marker.setMap(naverMap);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
