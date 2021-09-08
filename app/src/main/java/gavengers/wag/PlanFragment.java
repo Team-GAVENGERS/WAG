5f0e13d9a163c72250d8cd57545036b2acfe5333
SeungGun <saverrw2864@naver.com>
2021-09-08 12:21:38 +0900

    completed appoints of date range in calendar

--- app/src/main/java/gavengers/wag/PlanFragment.java
+++ app/src/main/java/gavengers/wag/PlanFragment.java
@@ -56,6 +56,7 @@ public class PlanFragment extends Fragment {
     private int height;
 
     @Nullable
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = (ViewGroup) inflater.inflate(R.layout.plan_layout, container, false);
@@ -94,7 +95,7 @@ public class PlanFragment extends Fragment {
         materialCalendarView.setArrowColor(Color.parseColor("#f1a53e"));
         materialCalendarView.setTitleAnimationOrientation(MaterialCalendarView.HORIZONTAL);
 
-        materialCalendarView.setTileHeight((int)(height * 0.11)); // 디바이스 전체 높이의 11%만큼 타일의 높이 설정
+        materialCalendarView.setTileHeight((int) (height * 0.11)); // 디바이스 전체 높이의 11%만큼 타일의 높이 설정
         /**
          *  ※기본 설정대로 했을 경우
          * 각 날마다의 보여지는 최대 일정 개수는 2개 (이론상 총 3개 {day + 일정들})
@@ -108,7 +109,7 @@ public class PlanFragment extends Fragment {
                 /**
                  * 각각의 날을 캘린더에 표시하는 formatting method
                  * 그 날짜의 일정 데이터를 가져오고 조건에 맞게 일정 데이터의 Title을 함께 출력력
-                */
+                 */
                 Integer[] tmp = compareDayIsThere(day); //
                 if (tmp.length == 1) { // 일정이 1개일 때
                     return day.getDay() + "\n" + infoList.get(tmp[0]).getAppointmentName();
@@ -156,6 +157,7 @@ public class PlanFragment extends Fragment {
                         for (DocumentSnapshot doc : task.getResult()) {
                             Appointment info = doc.toObject(Appointment.class);
                             if (info != null) {
+                                checkDateRange(info); // 시작 시간 ~ 종료 시간 범위 비교
                                 infoList.add(info);
                             } else {
                                 Log.d("appoint null", "data is null");
@@ -172,7 +174,16 @@ public class PlanFragment extends Fragment {
         });
     }
 
-    private void checkDateRange(Appointment info){
+    /**
+     * parameter 인 info 의 시작 날짜 ~ 종료 날짜 범위를 체크해서 일수 차이가 0이 아니라면
+     * infoList 에 시작시간만 다른 새로운 객체를 추가함
+     * author SeungGun
+     *
+     * @param info
+     */
+    private void checkDateRange(Appointment info) {
+        // 시작 시간 제외하고 추가하기
+        // 시작 시간은 compareDayIsThere 함수에서 추가하기 때문
         try {
             Date startDate = new SimpleDateFormat("yyyy-MM-dd").parse(info.getStartTime());
             Date endDate = new SimpleDateFormat("yyyy-MM-dd").parse(info.getEndTime());
@@ -180,17 +191,30 @@ public class PlanFragment extends Fragment {
             Calendar endCal = Calendar.getInstance();
             startCal.setTime(startDate);
             endCal.setTime(endDate);
-            long diffSec = (startCal.getTimeInMillis() - endCal.getTimeInMillis()) / 1000;
+            long diffSec = (endCal.getTimeInMillis() - startCal.getTimeInMillis()) / 1000;
             long diffDays = diffSec / (24 * 60 * 60); // 일 차이
-            for(long i=0; i<diffDays; ++i){
+
+            if (diffDays == 0) {
+                // 범위날짜가 없다는 의미로 함수 종료
+                return;
+            }
+
+            for (long i = 0; i < diffDays; ++i) {
                 // 반복하기
+                startCal.add(Calendar.DATE, 1);
+                String date = startCal.get(Calendar.YEAR) + "-" + getMonthsFormatting(startCal.get(Calendar.MONTH)) + "-" + getDaysFormatting(startCal.get(Calendar.DATE));
+                infoList.add(new Appointment(info.getAppointType(), info.getAppointmentName(), date,
+                        info.getEndTime(), info.getImportance(), info.getPlace(), info.getParticipants(), info.getMemoStr(), info.getWriterId(), info.getDocumentId()));
             }
         } catch (ParseException e) {
             e.printStackTrace();
         }
     }
+
     /**
      * parameter의 날짜와 일정 데이터 리스트의 날짜가 동일 할때 그 일정 object의 index를 기록해서 반환
+     * author SeungGun
+     *
      * @param day
      * @return parameter의 그 날짜에서 일정 데이터 리스트에 해당하는 index 정수 배열
      */
@@ -212,10 +236,22 @@ public class PlanFragment extends Fragment {
         }
     }
 
+    /**
+     * int 로 받아온 parameter month 를 문자열 형식에 맞게 변환해서 리턴하는 함수
+     *
+     * @param month
+     * @return
+     */
     public String getMonthsFormatting(int month) {
         return (month + 1) < 10 ? "0" + (month + 1) : (month + 1) + "";
     }
 
+    /**
+     * int 로 받아온 parameter day 를 문자열 형식에 맞게 변환해서 리턴하는 함수
+     *
+     * @param day
+     * @return
+     */
     public String getDaysFormatting(int day) {
         return day < 10 ? "0" + day : day + "";
     }
